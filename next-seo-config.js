26e3355a0e15e12dda377297c5b91ddee6f2159b
WesleyWong <wesley16838@gmail.com>
2023-03-27 01:11:25 -0500

    add meta tag

---
+++ next-seo-config.js
@@ -0,0 +1,17 @@
+export const NEXT_SEO_DEFAULT = {
+    title: 'Wesley Portfolio',
+    description: 'Weihsuan Wong\' Portfolio',
+    openGraph: {
+        url: 'https://wesley-portfolio.vercel.app/',
+        title: 'Wesley Portfolio',
+        description: ' Weihsuan Wong\' Portfolio',
+        images: [
+            {
+                url: './public/page-image.png',
+                alt: 'Og Image Alt',
+                type: 'image/png',
+            }
+        ],
+        siteName: 'SiteName',
+    }
+};
