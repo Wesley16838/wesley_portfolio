e459831c056b9d67607eada707b0d3443ad394b6
WesleyWong <wesley16838@gmail.com>
2024-02-25 15:19:08 -0600

    Update index.tsx

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,21 +1,24 @@
 import Props from "@/types/components/nav";
-import Image from 'next/image'
+import Image from "next/image";
 import "./style.scss";
 
 const Nav = ({ listItems, currentState }: Props) => {
-    return (
-        <ul className="side-nav">
-            {
-                listItems.map((item, index) => {
-                    return (
-                        <li key={`${item.title}${index}`} className={index === currentState ? "active" : "non-active"}>
-                            <div className="shortTitle">{item.shortTitle}</div>
-                        </li>
-                    )
-                })
-            }
-        </ul>
-    )
-}
+  return (
+    <ul className="side-nav">
+      {listItems !== null &&
+        currentState !== null &&
+        listItems.map((item, index) => {
+          return (
+            <li
+              key={`${item.title}${index}`}
+              className={index === currentState ? "active" : "non-active"}
+            >
+              <div className="shortTitle">{item.shortTitle}</div>
+            </li>
+          );
+        })}
+    </ul>
+  );
+};
 
-export default Nav
+export default Nav;
